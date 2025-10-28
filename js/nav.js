d984e28c2be100c489613c6832719147193bcc58
Quark <1378395929@qq.com>
2025-10-29 00:48:37 +0800

    111

--- js/nav.js
+++ js/nav.js
@@ -8,13 +8,13 @@ document.write(`
             </a>
             <div class="header-nav-container">
                 <ul class="header-nav">
-                    <li><a href="https://lsqkk.github.io/article-list">文章</a></li>
-                    <li><a href="https://lsqkk.github.io/tool">工具</a></li>
-                    <li><a href="https://lsqkk.github.io/games">游戏</a></li>
-                    <li><a href="https://lsqkk.github.io/a">实验室</a></li>
-                    <li><a href="https://lsqkk.github.io/daily">日报</a></li>
-                    <li><a href="https://lsqkk.github.io/qtv">视频</a></li>
-                    <li><a href="https://lsqkk.github.io/a/lyb" target="blank">留言</a></li>
+                    <li><a href="/article-list">文章</a></li>
+                    <li><a href="/tool">工具</a></li>
+                    <li><a href="/games">游戏</a></li>
+                    <li><a href="/a">实验室</a></li>
+                    <li><a href="/daily">日报</a></li>
+                    <li><a href="/qtv">视频</a></li>
+                    <li><a href="/a/lyb" target="blank">留言</a></li>
                     <li id="login-button"><a
                             href="https://github.com/login/oauth/authorize?client_id=Ov23liKnR1apo7atwzU0&redirect_uri=https://lsqkk.github.io/auth.html&scope=user">登录</a>
                     </li>
@@ -29,13 +29,13 @@ document.write(`
 
     <div class="nav">
         <ul class="mobile-nav">
-            <li><a href="https://lsqkk.github.io/article-list">文章</a></li>
-            <li><a href="https://lsqkk.github.io/tool">工具</a></li>
-            <li><a href="https://lsqkk.github.io/games">游戏</a></li>
-            <li><a href="https://lsqkk.github.io/a">实验室</a></li>
-            <li><a href="https://lsqkk.github.io/daily">日报</a></li>
-            <li><a href="https://lsqkk.github.io/qtv">视频</a></li>
-            <li><a href="https://lsqkk.github.io/a/lyb" target="blank">留言</a></li>
+            <li><a href="/article-list">文章</a></li>
+            <li><a href="/tool">工具</a></li>
+            <li><a href="/games">游戏</a></li>
+            <li><a href="/a">实验室</a></li>
+            <li><a href="/daily">日报</a></li>
+            <li><a href="/qtv">视频</a></li>
+            <li><a href="/a/lyb" target="blank">留言</a></li>
             <li id="mobile-login-button"><a
                     href="https://github.com/login/oauth/authorize?client_id=Ov23liKnR1apo7atwzU0&redirect_uri=https://lsqkk.github.io/auth.html&scope=user">登录</a>
             </li>
