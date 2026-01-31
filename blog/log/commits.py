33351e7b1ce1a64c4365d065820328c91e4747ed
Quark <1378395929@qq.com>
2026-01-31 13:48:38 +0800

    更新 - 更新日志

--- blog/log/commits.py
+++ blog/log/commits.py
@@ -65,8 +65,8 @@ def process_commits(commits, output_file='output_commits.txt'):
 
 def main():
     # 设置日期范围（可以修改这里的时间）
-    since_date = "2026-01-25"
-    until_date = "2026-01-28"
+    since_date = "2026-01-28"
+    until_date = "2026-01-31"
     
     # 或者使用命令行参数（如果提供了的话）
     if len(sys.argv) > 2:
